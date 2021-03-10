9bad779001d23297ef620c0e500d175f733ce7af
Jake Lamarre <jakelamarre@hotmail.com>
2021-03-10 02:26:32 -0500

    added form with styling 4.2.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,5 @@ var createTaskHandler = function() {
 
 buttonE1.addEventListener("click", createTaskHandler);
 
-// buttonE1.addEventListener("click", function() {
-//     var listItemE1 = document.createElement("li");
-//     listItemE1.className = "task-item";
-//     listItemE1.textContent = "This is a new task.";
-//     tasksToDoE1.appendChild(listItemE1);
-// });
+
 
